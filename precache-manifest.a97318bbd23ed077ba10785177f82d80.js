3d1c0f5678ccdb98ff6310947299d5132d2de46e
Suraj Singh <surajt9755@gmail.com>
2020-03-22 23:18:22 +0530

    Updates

---
+++ precache-manifest.a97318bbd23ed077ba10785177f82d80.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cc35134dd2e55199d8c503bc1735d76",
+    "url": "/Coronavirus/index.html"
+  },
+  {
+    "revision": "0dac7123ed52f52d96c5",
+    "url": "/Coronavirus/static/css/main.a728598f.chunk.css"
+  },
+  {
+    "revision": "f776ac0cf82fa5891605",
+    "url": "/Coronavirus/static/js/2.afa57cd8.chunk.js"
+  },
+  {
+    "revision": "bf0ee5bd53566821287876a78c14204d",
+    "url": "/Coronavirus/static/js/2.afa57cd8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0dac7123ed52f52d96c5",
+    "url": "/Coronavirus/static/js/main.6fdcf903.chunk.js"
+  },
+  {
+    "revision": "ff66e56fa9a79b6fae1b",
+    "url": "/Coronavirus/static/js/runtime-main.ed044fe5.js"
+  }
+]);
